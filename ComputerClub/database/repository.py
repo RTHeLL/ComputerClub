5d1ffc18b53a1e625bb001974ff182eb43ea11a7
RTHeLL <k1ndermail@ya.ru>
2021-12-17 18:55:29 +0300

    Update 0.0.6 Add profile_handler in UCP blueprint. For get user profile /ucp/user/<user_id> Add new columns in User model (favorite_game, site_link, steam, Twitter, instagram, facebook, about_me, last_session) Add get_avatar method in User model Add 404 page (404.html, 404.css) Add profile_user(self, payload) method in UsersController. This method return __payload with user and title. Add before_request route for autoupdate last_session date

--- ComputerClub/database/repository.py
+++ ComputerClub/database/repository.py
@@ -1,8 +1,8 @@
-from sqlalchemy.exc import SQLAlchemyError
-
 from ComputerClub import db
 
 from ComputerClub.models import News, User
+
+from decorators import db_commit
 
 
 class Repository:
@@ -20,30 +20,29 @@ class UsersRepository(Repository):
         __user = User.query\
                     .filter(self.__payload_definition(_payload=payload))\
                     .first()
+
         return __user
 
+    @db_commit
     def create_user(self, payload):
-        try:
-            __user = User(username=payload.username.data,
-                          email=payload.email.data,
-                          first_name=payload.first_name.data,
-                          last_name=payload.last_name.data)
-            __user.generate_password(payload.password.data)
-            db.session.add(__user)
-            db.session.commit()
-            return True
-        except SQLAlchemyError as e:
-            print(e)
-            db.session.rollback()
-            return False
+        __user = User(username=payload.username.data,
+                      email=payload.email.data,
+                      first_name=payload.first_name.data,
+                      last_name=payload.last_name.data)
+        __user.generate_password(payload.password.data)
+        db.session.add(__user)
 
     @staticmethod
     def __payload_definition(_payload):
+        __query = 0
         if _payload is None:
-            return User.id is not None
+            __query = User.id is not None
 
-        if 'username' in _payload:
-            return User.username == _payload['username']
+        if 'user_id' in _payload:
+            __query = User.id == _payload['user_id']
+        elif 'username' in _payload:
+            __query = User.username == _payload['username']
         elif 'email' in _payload:
-            return User.email == _payload['email']
+            __query = User.email == _payload['email']
 
+        return __query
